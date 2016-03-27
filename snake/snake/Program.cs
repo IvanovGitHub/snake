b47fbef0554d8e763aba920ae431c80ad15e1557
Sergey <ivanov_sergey92@mail.ru>
2016-03-27 15:26:13 +0300

    Класс змейка

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -12,17 +12,21 @@ namespace snake
         {
             Console.SetBufferSize(80, 25);
 
-            HorizontalLine line1 = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine line1 = new HorizontalLine(0, 78, 0, '#');
             line1.Draw();
 
-            HorizontalLine line2 = new HorizontalLine(0, 78, 24, '+');
+            HorizontalLine line2 = new HorizontalLine(0, 78, 24, '#');
             line2.Draw();
 
-            VerticalLine line3 = new VerticalLine(0, 24, 0, '+');
+            VerticalLine line3 = new VerticalLine(0, 24, 0, '#');
             line3.Draw();
 
-            VerticalLine line4 = new VerticalLine(0, 24, 78, '+');
+            VerticalLine line4 = new VerticalLine(0, 24, 78, '#');
             line4.Draw();
+
+            Point p = new Point(4, 5, 'O');
+            snake snake = new snake(p, 4, Direction.Down);
+            snake.Draw();
 
             Console.ReadLine();
         }
